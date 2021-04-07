8fdbf6341347fdcbe921d9cd9b77809d3fa55122
Niraj <sharma@neera.com>
2021-04-07 14:50:39 +0530

    data-types

---
+++ Jss_basics/basics/script.js
@@ -0,0 +1,15 @@
+console.log(123432,typeof 123432)
+console.log(123432.344,typeof 123432.344)
+console.log('a',typeof 'a')
+console.log('asdfgh',typeof 'asdfgh')
+console.log(true,typeof true)
+console.log(null,typeof null)
+console.log(undefined,typeof undefined)
+
+function a()
+{
+    return 1
+}
+
+console.log(a , typeof a)
+console.log(a() , typeof a())
